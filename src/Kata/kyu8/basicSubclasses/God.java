9f2fab50eceef2709775265e03034381e8038eca
Vendoros <Good0908>
2019-10-14 03:01:56 +0800

    0.0.0.24 -> update BasicSubclasses

--- src/Kata/kyu8/basicSubclasses/God.java
+++ src/Kata/kyu8/basicSubclasses/God.java
@@ -1,17 +1,40 @@
 package Kata.kyu8.basicSubclasses;
 
 public class God {
-    public static Human[] create() {
-        return new Human[]{new Man(), new Woman()};
+    public static Human[] create(){
+        Man adam = new Man("adam", 25, 100);
+        Woman eve = new Woman("eve", 25, 100);
+
+        Human[] array = {adam, eve};
+        return array;
     }
 }
 
-class Human {
+abstract class Human{
+    private String name;
+    private int age;
+
+    public Human(String n, int a){
+        name = n;
+        age = a;
+    }
 }
 
-class Man extends Human {
+class Man extends Human{
+    private int strength;
+
+    public Man(String n, int a, int s){
+        super(n, a);
+        strength = s;
+    }
 }
 
-class Woman extends Human {
+class Woman extends Human{
+    private int love;
+
+    public Woman (String n, int a, int l){
+        super(n, a);
+        love = l;
+    }
 }
 
